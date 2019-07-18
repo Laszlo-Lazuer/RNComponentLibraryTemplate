3d76a9d2b0ffef2ac8751082d51e19569f29e928
Laszlo Lazuer <visualjazz@gmail.com>
2019-07-18 10:06:36 -0500

    chore(npm) - adds example components, as well as npm publishing configs.

---
+++ src/Components/ComponentOne/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const ComponentOne = props => (
+    <View style={{ flexDirection: 'row', marginTop: 90 }}>
+        <Text>Example ComponentOne!</Text>
+    </View>
+);
+
+export default ComponentOne;
---
+++ src/Components/ComponentTwo/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const ComponentTwo = props => (
+    <View style={{ flexDirection: 'row', marginTop: 90 }}>
+        <Text>Example ComponentTwo!</Text>
+    </View>
+);
+
+export default ComponentTwo;
